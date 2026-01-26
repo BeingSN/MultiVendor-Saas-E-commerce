e5a39b5b7f6386299e0bf50f4d42a1f4e9744e19
Mohammad Shahmeer <shahmir.nazir@gmail.com>
2026-01-26 21:15:45 +0500

    working

--- apps/auth-service/src/controller/auth-controller.ts
+++ apps/auth-service/src/controller/auth-controller.ts
@@ -1,14 +1,20 @@
 //Register a new user
 
 import { Request, Response, NextFunction } from "express";
-import { ValidationError } from "../../../../packages/middleware/error-handler/index.js";
+import {
+  AuthenticationError,
+  ValidationError,
+} from "../../../../packages/middleware/error-handler/index.js";
 import {
   checkOtpRestrictions,
   sendOtp,
   trackOtpRequests,
   validateRegisrationData,
+  verifyOtp,
 } from "../utils/auth.helper.js";
 import prisma from "../../../../packages/libs/prisma/index.js";
+import bcrypt from "bcryptjs";
+import { setCookie } from "../utils/cookies/setCookie.js";
 
 export const userRegistration = async (
   req: Request,
@@ -33,5 +39,95 @@ export const userRegistration = async (
       .json({ message: "OTP sent to email please verify your account." });
   } catch (err) {
     return next(err);
+  }
+};
+
+export const verifyUser = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
+    const { email, otp, password, name } = req.body;
+
+    if (!email || !otp || !password || !name) {
+      return next(new ValidationError("All fields are requried!"));
+    }
+
+    const existingUser = await prisma.user.findUnique({ where: { email } });
+
+    if (existingUser) {
+      return next(new ValidationError("User already exist"));
+    }
+    await verifyOtp(email, otp, next);
+
+    const hashedPassword = await bcrypt.hash(passowrd, 10);
+
+    const user = prisma.user.create({
+      data: { name, email, password, hashedPassword },
+    });
+
+    res
+      .status(201)
+      .json({ success: true, message: "User registered successfully!" });
+  } catch (err) {
+    return next(error);
+  }
+};
+
+export const loginUser = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return next(new ValidationError("Email and password are required"));
+    }
+
+    const user = await prisma.user.findUnique({
+      where: { email },
+    });
+
+    if (!user) {
+      return next(new AuthenticationError("Invalid email or password"));
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return next(new AuthenticationError("Invalid email or password"));
+    }
+
+    //Generate access token and refresh token
+
+    const accessToken = jwt.sign(
+      { id: user.id, role: "User" },
+      proccess.env.ACCESS_TOKEN_SECRET as string,
+      {
+        expiresIn: "15m",
+      },
+    );
+
+    const refreshToken = jwt.sign(
+      { id: user.id, role: "User" },
+      proccess.env.REFRESH_TOKEN_SECRET as string,
+      {
+        expiresIn: "7d",
+      },
+    );
+
+    //store the refresh and access token in an httpOnly secure cookie
+    setCookie(res, "refresh-token", refreshToken);
+    setCookie(res, "access-token", accessToken);
+
+    res.status(200).json({
+      message: "Login ssuccessful!",
+      user: { id: user.id, email: user.email, name: user.name },
+    });
+  } catch (error) {
+    return next(error);
   }
 };
--- apps/auth-service/src/routes/auth-router.ts
+++ apps/auth-service/src/routes/auth-router.ts
@@ -1,8 +1,14 @@
 import express, { Router } from "express";
-import { userRegistration } from "../controller/auth-controller.js";
+import {
+  loginUser,
+  userRegistration,
+  verifyUser,
+} from "../controller/auth-controller.js";
 
 const router: Router = express.Router();
 
 router.post("/user-registration", userRegistration);
+router.post("/verify-user", verifyUser);
+router.post("/login-user", loginUser);
 
 export default router;
